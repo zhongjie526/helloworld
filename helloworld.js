b2e29c2a81fac3240053419d2a974187928838a1
Frank Zhang <zzj.anonymous@gmail.com>
2019-10-11 17:55:48 +0800

    testing codepipeline

--- helloworld.js
+++ helloworld.js
@@ -8,7 +8,7 @@ http.createServer(function (request, response) {
    response.writeHead(200, {'Content-Type': 'text/plain'})
 
    // Send the response body as "Hello World"
-   response.end('Hello World\n')
+   response.end('Hello Frank\n')
 }).listen(3000)
 
 // Console will print the message
